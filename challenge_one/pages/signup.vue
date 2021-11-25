f831b7f0fd2d27b612776177f4f7c72bd90aaaf0
Fondao Sriworasa <fondao21011999@gmail.com>
2021-11-25 22:33:35 +0700

    add page view sign up

---
+++ challenge_one/pages/signup.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="background">
+    <div><h1>Hello!!!Sign up</h1></div>
+      
+</div>
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.background {
+    background-color: aquamarine;
+}
+
+</style>
