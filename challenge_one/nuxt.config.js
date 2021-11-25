99264dc64aaf087c34f919da9c294ba2d0e68f6d
Fondao Sriworasa <fondao21011999@gmail.com>
2021-11-25 22:33:56 +0700

    Add sass

--- challenge_one/nuxt.config.js
+++ challenge_one/nuxt.config.js
@@ -61,5 +61,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    loaders: {
+      sass: {
+        implementation: require('sass'),
+      },
+      scss: {
+        implementation: require('sass'),
+      },
+    },
   }
 }
