1264bf2742f27a8339eeb01ab5cdc0ccf0a9e15e
Fondao Sriworasa <fondao21011999@gmail.com>
2021-11-25 21:56:12 +0700

    add Global Settings

--- challenge_one/nuxt.config.js
+++ challenge_one/nuxt.config.js
@@ -44,6 +44,21 @@ export default {
     'bootstrap-vue/nuxt',
   ],
 
+  // ------------------
+  // Global Settings : https://nuxtjs.org/docs/features/configuration/#global-settings
+  script: [
+    {
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'
+    }
+  ],
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap'
+    }
+  ],
+  // -------------------
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
