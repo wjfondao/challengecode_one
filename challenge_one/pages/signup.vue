f95af91fca01b5657969cb0fe1cde6eb15517a98
Fondao Sriworasa <fondao21011999@gmail.com>
2021-11-25 23:01:29 +0700

    add background color

--- challenge_one/pages/signup.vue
+++ challenge_one/pages/signup.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="background">
-    <div><h1>Hello!!!Sign up</h1></div>
+    <div>
+        <h1>Hello!!!Sign up</h1>
+    </div>
       
 </div>
 
@@ -11,7 +13,7 @@ export default {}
 </script>
 
 <style scoped lang="scss">
-.background {
+html{
     background-color: aquamarine;
 }
 
