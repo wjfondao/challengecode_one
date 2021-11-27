b1e25cfe5ecd7e126148367e7fb3b80890b7406c
Fondao Sriworasa <fondao21011999@gmail.com>
2021-11-27 23:08:30 +0700

    add responsive in view

--- challenge_one/pages/signup.vue
+++ challenge_one/pages/signup.vue
@@ -1,23 +1,25 @@
 <template>
-<div class="container row">
-
-    <div class="partone col-xl-5 col-lg-12 col-md-12 col-sm-12"> 
+  <div class="container row">
+    <div class="partone col-xl-5 col-lg-12 col-md-12 col-sm-12">
       <div class="imagemylogo">
-        <img class="side-img" alt="Vue logo" width="80px" height="80px" src="../assets/images/logo1.png" />
+        <img
+          class="side-img"
+          alt="Vue logo"
+          width="80px"
+          height="80px"
+          src="../assets/images/logo1.png"
+        />
       </div>
       <div class="texttitle mt-5">
-        <p style="font-size:48px; font-weight: bold;">Hey,</p>
-        <p style="font-size:48px; font-weight: bold;">it’s Christmas time</p>
-        <h4>Events & gifts by wjbenz</h4>
+        <p id="textfirst">Hey,</p>
+        <p id="textfirst">it’s Christmas time</p>
+        <p id="textsecond">Events & gifts by wjbenz</p>
       </div>
-
     </div>
     <div class="parttwo col-xl-7 col-lg-12 col-md-12 col-sm-12">
-      <h1>hello</h1>
+      <Cardsignup />
     </div>
-      
-</div>
-
+  </div>
 </template>
 
 <script>
@@ -35,33 +37,58 @@ export default {}
   background-attachment: fixed;
   background-position: bottom;
   background-size: 100%;
-  background-color: #A5D7CC;
+  background-color: #a5d7cc;
 
   .partone {
     padding: 10px;
 
-    .texttitle{
+    .texttitle {
       padding-left: 5rem;
       color: white;
-      font-family: Lucida Bright,Georgia,serif; 
+      font-family: Lucida Bright, Georgia, serif;
+      font-weight: bold;
+
+      /* Iphone */
+      @media only screen and (min-device-width: 320px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+        padding-left: 2rem;
+      }
+
+      #textfirst {
+        font-size: 48px;
+
+        /* Iphone */
+        @media only screen and (min-device-width: 320px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+          font-size: 24px;
+        }
+      }
+      #textsecond {
+        font-size: 24px;
+
+        /* Iphone */
+        @media only screen and (min-device-width: 320px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+          font-size: 14px;
+        }
+      }
     }
   }
 
+  .parttwo {
+    padding: 0;
+
+    /* Ipadpro */
+    // @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) and (-webkit-min-device-pixel-ratio: 2) {
+    //   padding-top: 18rem;
+    // }
+
+    /* Ipad */
+    @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 1) {
+      padding-top: 11rem;
+    }
+
+    /* Iphone */
+    @media only screen and (min-device-width: 320px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+      padding-top: 6rem;
+    }
+  }
 }
-
-/* Ipadpro */
-//   @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) and (-webkit-min-device-pixel-ratio: 2) {
-
-//   }
-
-  /* Ipad */
-//   @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 1) {
-
-//   }
-
-  /* Iphone */
-//   @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
-  
-//   }
-
 </style>
