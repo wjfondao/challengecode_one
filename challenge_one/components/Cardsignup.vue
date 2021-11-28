ca027836ed96458b5c7c1ef6f812334ea3530735
Fondao Sriworasa <fondao21011999@gmail.com>
2021-11-28 21:28:51 +0700

    add text input

--- challenge_one/components/Cardsignup.vue
+++ challenge_one/components/Cardsignup.vue
@@ -1,46 +1,89 @@
 <template>
   <div class="containercard">
-    <b-card class="maincard" title="Card title" sub-title="Card subtitle">
-      <b-card-text>
-        Some quick example text to build on the <em>card title</em> and make up
-        the bulk of the card's content.
-      </b-card-text>
+    <!-- Star Card Sign up -->
+    <b-card class="maincard">
+      <!-- Star Form Group -->
+      <div class="formsignup">
+        <!--Title text -->
+        <p id="title">Sign up</p>
+        <div class="fullname row">
+          <div class="col-xl-6 col-lg-6col-md-6 col-sm-12">
+            <p>Firstname</p>
+            <b-form-input
+              v-model="text"
+              placeholder="Enter your name"
+            ></b-form-input>
+          </div>
+          <div class="col-xl-6 col-lg-6col-md-6 col-sm-12">
+            <p>Lastname</p>
+            <b-form-input
+              v-model="text"
+              placeholder="Enter your name"
+            ></b-form-input>
+          </div>
+        </div>
 
-      <b-card-text>A second paragraph of text in the card.</b-card-text>
-
-      <a href="#" class="card-link">Card link</a>
-      <b-link href="#" class="card-link">Another link</b-link>
+        <!-- End Form Group -->
+      </div>
     </b-card>
+    <!-- End Card Sign up -->
   </div>
 </template>
 
 <style scoped lang="scss">
-.maincard{
-    min-height: 100vh;
-    min-width: -webkit-fill-available;
-    background-color: white;
-    border-top-left-radius: 2.5rem;
-    border-bottom-left-radius: 2.5rem;
-    border: none;
-    
+.maincard {
+  min-height: 100vh;
+  min-width: -webkit-fill-available;
+  background-color: white;
+  border-top-left-radius: 2.5rem;
+  border-bottom-left-radius: 2.5rem;
+  border: none;
+
+  /* Ipadpro */
+  @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) and (-webkit-min-device-pixel-ratio: 2) {
+    border-top-right-radius: 2.5rem;
+    border-bottom-left-radius: 0 !important;
+  }
+
+  /* Ipad */
+  @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 1) {
+    border-top-right-radius: 2.5rem;
+    border-bottom-left-radius: 0 !important;
+  }
+
+  /* Iphone */
+  @media only screen and (min-device-width: 320px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+    border-top-right-radius: 2.5rem;
+    border-bottom-left-radius: 0 !important;
+  }
+
+  .formsignup {
+    padding: 8rem 10rem 0 10rem;
+
+    #title {
+      font-size: 48px;
+      font-weight: bold;
+
+      /* Iphone */
+      @media only screen and (min-device-width: 320px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+        font-size: 24px;
+      }
+    }
+
     /* Ipadpro */
     @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) and (-webkit-min-device-pixel-ratio: 2) {
-        border-top-right-radius: 2.5rem;
-        border-bottom-left-radius: 0 !important;
+      padding: 3rem 10rem 0 10rem;
     }
 
-    
     /* Ipad */
     @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 1) {
-       border-top-right-radius: 2.5rem;
-       border-bottom-left-radius: 0 !important;
+      padding: 3rem 10rem 0 10rem;
     }
 
-
-     /* Iphone */
+    /* Iphone */
     @media only screen and (min-device-width: 320px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
-       border-top-right-radius: 2.5rem;
-       border-bottom-left-radius: 0 !important;
+      padding: 2rem 2rem 0 2rem;
     }
+  }
 }
 </style>
