e8db6af58ebd2342de6ed66bd58fcfda221a8911
Fondao Sriworasa <fondao21011999@gmail.com>
2021-11-28 22:34:24 +0700

    install stylelint

--- challenge_one/stylelint.config.js
+++ challenge_one/stylelint.config.js
@@ -5,5 +5,17 @@ module.exports = {
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  "plugins": [
+    "stylelint-scss"
+  ],
+  "rules": {
+    // recommended rules
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+
+    // any other rules you'd want to change e.g.
+    "scss/dollar-variable-pattern" : "^foo",
+    "scss/selector-no-redundant-nesting-selector": true,
+  }
+  
 }
