8a6575360bd433b2448071dac39ed343a65614c6
Fondao Sriworasa <fondao21011999@gmail.com>
2021-12-31 00:14:18 +0700

    update view home

--- challenge_one/pages/home.vue
+++ challenge_one/pages/home.vue
@@ -1,10 +1,57 @@
 <template>
-    <div class="container row">
-        <div class="col-xl-8"> 
-            <h1>Page 1</h1>
-        </div>
-        <div class="col-xl-4">
-            <h1>Page 2</h1>
-        </div>
+  <div class="container">
+    <div class="pageone">
+      <div class="imagebell">
+        <img class="bell" src="../assets/images/bell.png" />
+      </div>
+      <!--  start: text marry christmas -->
+      <div class="textitle">
+        <p>Marry</p>
+        <p>Christmas</p>
+      </div>
     </div>
+    <div class="pagetwo">
+      <h1>Page 2</h1>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  min-height: 100vh;
+  min-width: -webkit-fill-available;
+  margin: 0;
+  flex-direction: row;
+  background-color: #a5d7cc;
+  font-family: Lucida Bright, Georgia, serif;
+  // Start : Page one
+  .pageone {
+    flex-basis: calc(50%) !important;
+    order: 1 !important;
+
+    .imagebell {
+      display: flex;
+      justify-content: center;
+      .bell {
+        width: 100px;
+        height: 150px;
+      }
+    }
+    .textitle {
+      font-weight: bold;
+      font-size: 80px;
+      color: white;
+      text-align: center;
+      p{
+          margin-bottom: -1rem;
+      }
+    }
+  }
+  .pagetwo {
+    flex-basis: calc(50%) !important;
+    order: 2 !important;
+  }
+}
+</style>
