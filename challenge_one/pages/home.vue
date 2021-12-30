f183d8b2af9b06f21643933cbc50b08da9c69759
Fondao Sriworasa <fondao21011999@gmail.com>
2021-12-30 23:08:29 +0700

    update home.vue

--- challenge_one/pages/home.vue
+++ challenge_one/pages/home.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="container">
-        <h1>Hi!!!  My home</h1>
+    <div class="container row">
+        <div class="col-xl-8"> 
+            <h1>Page 1</h1>
+        </div>
+        <div class="col-xl-4">
+            <h1>Page 2</h1>
+        </div>
     </div>
 </template>
