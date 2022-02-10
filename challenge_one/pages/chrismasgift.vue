9a5147665ddc2f02ea1ec02862c613c366e45791
Fondao Sriworasa <fondao21011999@gmail.com>
2022-02-10 18:57:21 +0700

    update loading in chrismassgift

--- challenge_one/pages/chrismasgift.vue
+++ challenge_one/pages/chrismasgift.vue
@@ -15,10 +15,12 @@
       </div>
     </div>
     <div class="loading">
-      <div class="imagesanta">
-        <img class="santa" src="../assets/images/santastandingonsled.png" />
+      <div class="flex">
+        <div class="imagesanta">
+          <img class="santa" src="../assets/images/santastandingonsled.png" />
+        </div>
+        <div class="bar"></div>
       </div>
-      <div class="bar "></div>
     </div>
   </div>
 </template>
@@ -35,18 +37,31 @@
   justify-content: center;
   align-items: center;
   z-index: 15;
-  animation: fadeout 10s linear forwards;
+  animation: fadeout 8s linear forwards;
+
+  /* Iphone */
+  @media only screen and (min-device-width: 280px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+    animation: fadeout 7s linear forwards;
+  }
 
   .imagesanta {
+    box-sizing: border-box;
     .santa {
-      position: absolute;
-      margin-top: 10rem;
+      display: flex;
+      position: relative;
+      align-items: center;
       width: 250px;
       height: 300px;
       animation-name: santa;
-      animation-duration: 15s;
+      animation-duration: 8s;
       animation-iteration-count: 1;
-      box-sizing: border-box;
+
+      /* Iphone */
+      @media only screen and (min-device-width: 280px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+        width: 150px;
+        height: 200px;
+        animation-duration: 15s;
+      }
     }
   }
 
@@ -56,11 +71,11 @@
 
   .bar {
     position: relative;
-    width: 1400px;
+    width: 2000px;
     height: 40px;
     background: transparent;
     margin: 0 25px;
-    border: 2px solid #a5d7cc;
+    border: 2px solid #fff;
     box-sizing: border-box;
 
     &::before {
@@ -70,13 +85,18 @@
       left: 0;
       width: 100%;
       height: 100%;
-      background: #a5d7cc;
+      background: #fff;
       transform-origin: left;
       animation: animate 5s linear forwards;
+
+      /* Iphone */
+      @media only screen and (min-device-width: 280px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+        animation: animate 8s linear forwards;
+      }
     }
 
     &::after {
-      content: 'Ho Ho Ho ~ Merry Christmas...';
+      content: ' Yo-Ho-Ho! ~ Merry Christmas...';
       position: absolute;
       top: 0;
       left: 0;
@@ -87,9 +107,23 @@
       letter-spacing: 10px;
       text-align: center;
       line-height: 36px;
-      color: #a5d7cc;
+      color: #a5d7cc !important;
       font-size: 20px;
       mix-blend-mode: difference;
+
+      /* Iphone */
+      @media only screen and (min-device-width: 280px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+        content: 'Merry Christmas...';
+        letter-spacing: 5px;
+        font-size: 12px;
+      }
+
+      /* Iphone */
+      @media only screen and (max-device-width: 280px) {
+        content: 'Merry Christmas...';
+        letter-spacing: 4px;
+        font-size: 9px;
+      }
     }
   }
 }
