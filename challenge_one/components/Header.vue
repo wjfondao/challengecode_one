f18f9cc052acc1688221ae988bd5ee980c47e994
Fondao Sriworasa <fondao21011999@gmail.com>
2022-02-11 22:32:36 +0700

    creat component header

---
+++ challenge_one/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+      <div>
+      <a href="../../">
+        <img
+          class="side-img"
+          alt="Vue logo"
+          width="80px"
+          height="80px"
+          src="../assets/images/logo1.png"
+        />
+      </a>
+    </div>
+</template>
