785cea2de2f1310b6977b7584c3d81edc6a08e8e
Fondao Sriworasa <fondao21011999@gmail.com>
2022-02-17 23:13:45 +0700

    add navbar

--- challenge_one/components/Header.vue
+++ challenge_one/components/Header.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="header">
-    <a href="../../" class="imglogo">
-      <img
-        class="side-img ml-xl-4 mt-xl-1 ml-1"
-        alt="Vue logo"
-        width="80px"
-        height="80px"
-        src="../assets/images/logo1.png"
-      />
-    </a>
+    <b-navbar variant="faded">
+      <b-navbar-brand href="#">
+        <a href="../../" class="imglogo">
+          <img
+            class="side-img ml-xl-4 mt-xl-1 ml-1"
+            alt="Vue logo"
+            width="80px"
+            height="80px"
+            src="../assets/images/logo1.png"
+          />
+        </a>
+      </b-navbar-brand>
+    </b-navbar>
   </div>
 </template>
 
